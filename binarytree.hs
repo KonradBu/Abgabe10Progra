aae46f7f651d1bf6f7167ad259629a95e50057fc
Hannizio <153449495+Hannizio@users.noreply.github.com>
2024-01-08 20:43:41 +0100

    Update binarytree.hs

--- binarytree.hs
+++ binarytree.hs
@@ -1,3 +1,6 @@
 import Text.Show.Functions
 
-data BinTree a b = B | Node a (BinTree a b) (BinTree a b)
+data BinTree a b = B | Node a (BinTree a b) (BinTree a b) deriving Show
+
+example :: BinTree (Int -> Bool) Char
+example = 
