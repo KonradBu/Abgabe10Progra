4c0f75158ad5fda63aa5a159ac884f996bdd429a
KonradBu <konradburgi1@gmail.com>
2024-01-08 21:48:32 +0100

    .

--- binarytree.hs
+++ binarytree.hs
@@ -4,3 +4,8 @@ data BinTree a b = Leaf b | Node a (BinTree a b) (BinTree a b) deriving Show
 
 example :: BinTree (Int -> Bool) Char
 example = Node (\x -> x > 4) (Node (\x -> x*x > x) (Leaf 'g') (Node (\x -> x == 0) (Leaf 'u') (Leaf 'l'))) (Node (\x -> x > 4) (Leaf 'f') (Leaf 'i'))
+
+
+countInnerNode :: BinTree()() -> Int
+countInnerTree Leaf x = 0
+countInnerNode = 1 + countInnerNode(Bintree (a) (b))
