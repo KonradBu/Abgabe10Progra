401c3cc29f1051de60d6e9d2bc88276d0e9098d7
KonradBu <konradburgi1@gmail.com>
2024-01-08 21:36:11 +0100

    .

--- binarytree.hs
+++ binarytree.hs
@@ -1,7 +1,6 @@
-{-# LANGUAGE BlockArguments #-}
 import Text.Show.Functions
 
 data BinTree a b = Leaf b | Node a (BinTree a b) (BinTree a b) deriving Show
 
 example :: BinTree (Int -> Bool) Char
-example = Node x -> x > 4 (x -> x*x == x ('g') (x -> == 0('u') ('l'))) (x -> x >= 7('f')('i'))
+example = Node (\x -> x > 4) (Node (\x -> x*x > x) (Leaf 'g') (Node (\x -> x == 0) (Leaf 'u') (Leaf 'l'))) (Node (\x -> x > 4) (Leaf 'f') (Leaf 'i'))
